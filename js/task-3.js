260809a669d796045b3feb5d1748e62fad07db6a
Chernysh Illya <olips1988@gmail.com>
2020-08-20 11:10:48 +0300

    1
    1

---
+++ js/task-3.js
@@ -0,0 +1,44 @@
+// Напиши скрипт имитирующий авторизацию администратора в панели управления.
+
+// Есть переменная message в которую будет записано сообщение о результате. При загрузке страницы у посетителя запрашивается пароль через prompt:
+
+// Если нажали Cancel, записать в message строку 'Отменено пользователем!'
+// В протовном случае, если введен пароль который совпадает со значением константы ADMIN_PASSWORD, записать в message строку 'Добро пожаловать!'
+// В противном случае, то есть если ни одно из предыдущих условий не выполнилось, записать в message строку 'Доступ запрещен, неверный пароль!'
+// После всех проверок вывести в alert значение переменной message.
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// let message;
+
+let userPassword = prompt('Введите пароль для авторизации');
+
+
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+let message;
+
+// Write code under this line
+if (userPassword === ADMIN_PASSWORD) {
+  message = ACCESS_IS_ALLOWED;
+} else if (userPassword === null) {
+  message = CANCELED_BY_USER;
+} else {
+  message = ACCESS_DENIED;
+}
+
+console.log(message);
+
+//если userPassword равно 'jqueryismyjam'
+// то значение message будет равно
+// 'Добро пожаловать!'
+
+//если userPassword равно  null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если userPassword равно '123'
+// то значение message будет равно
+// 'Доступ запрещен, неверный пароль!'
---
+++ js/task-5.js
@@ -0,0 +1,66 @@
+// Пользователь может оформить доставку товара к себе в страну, указав ее при посещении страницы в prompt. Учти, пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'.
+
+// Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну. Обязательно используй switch. Формат сообщения: 'Доставка в [страна] будет стоить [цена] кредитов'.
+
+// Но доставка есть не везде, если указанной страны нет в списке, то выводи в alert сообщение 'В вашей стране доставка не доступна'.
+
+// Ниже приведен список стран и стоимость доставки.
+
+// Китай - 100 кредитов
+// Чили - 250 кредитов
+// Австралия - 170 кредитов
+// Индия - 80 кредитов
+// Ямайка - 120 кредитов
+
+let countryName = prompt('Введите страну для доставки?');
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+const CHINA = "Китай";
+const AUSTRALIA = "Австралия";
+const INDIA = "Индия";
+const JAMAICA = "Ямайка";
+let message;
+let price = 0;
+let country;
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.toLowerCase().slice(1); // Write code on this line
+  switch (country) {
+    // Write code under this line
+    case CHINA:
+      price = 100;
+      break;
+    case AUSTRALIA:
+      price = 170;
+      break;
+    case INDIA:
+      price = 80;
+      break;
+    case JAMAICA:
+      price = 120;
+      break;
+
+    default:
+      message = NO_DELIVERY;
+  }
+}
+if (price > 0) {
+  // Write code on this line
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+}
+
+console.log(message);
+
+//если countryName равно "КитаЙ"
+// то значение message будет равно
+// 'Доставка в Китай будет стоить 100 кредитов'
+
+//если countryName равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если countryName равно "Чили"
+// то значение message будет равно
+// 'В выбранную страну доставка недоступна.'
