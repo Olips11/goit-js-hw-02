7b641c2c871a526f5a1dc484dd3b043450e88cfb
Chernysh Illya <olips1988@gmail.com>
2020-08-25 19:29:55 +0300

    1

--- js2/task2.js
+++ js2/task2.js
@@ -57,31 +57,31 @@
 
 // pop - удаляет один элемент в конце масива
 
-const stack = []; 
+// const stack = []; 
 
-//добавляем элемент в конце масива push
+// //добавляем элемент в конце масива push
 
-stack.push(1);
-console.log(stack); // [1]
+// stack.push(1);
+// console.log(stack); // [1]
 
-stack.push(2);
-console.log(stack); //  [1, 2]
+// stack.push(2);
+// console.log(stack); //  [1, 2]
 
-stack.push(3);
-console.log(stack); // [1, 2, 3]
+// stack.push(3);
+// console.log(stack); // [1, 2, 3]
 
-//удаляем элемент в конце масива pop
+// //удаляем элемент в конце масива pop
 
-console.log(stack.pop());  // 3
-console.log(stack);        // [1, 2]
+// console.log(stack.pop());  // 3
+// console.log(stack);        // [1, 2]
 
-console.log(stack.pop()); // 2
-console.log(stack)        // [1]
+// console.log(stack.pop()); // 2
+// console.log(stack)        // [1]
 
-console.log(stack.pop()); // 1
-console.log(stack);       // []
+// console.log(stack.pop()); // 1
+// console.log(stack);       // []
 
-console.log(stack.pop()); // undefined
+// console.log(stack.pop()); // undefined
 
 // shift - удаляет элемент из начала массива
 
@@ -97,7 +97,76 @@ console.log(stack.pop()); // undefined
                       // 5 - Метод slice()
 
 
-const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
 
-//  вернет новый масив в котором будут элементы с индексами от 1 до 2
+// //  вернет новый масив в котором будут элементы с индексами от 1 до 2
 
+// console.log(clients.slice(1,3)); //["Ajax", "Poly"]
+
+// console.log(clients.slice(1)); //  ["Ajax", "Poly", "Kiwi"]
+
+// console.log(clients.slice());  // ["Mango", "Ajax", "Poly", "Kiwi"]
+
+// console.log(clients.slice(-2));  //["Poly", "Kiwi"]
+
+
+                        //Удаление элиментов массива splice
+
+// const scores = [1, 2, 3, 4, 5,]
+
+// const deletedScores = scores.splice(0, 3);
+
+// console.log(scores); //[4, 5]
+
+// // удаленные элементы
+
+// console.log(deletedScores);  //[1, 2, 3]
+
+//                         // Вставка элементов в массив
+
+// const colors = ['red', 'green', 'blue'];
+
+// colors.splice(2, 0, 'purpl');
+
+// console.log(colors); //["red", "green", "purpl", "blue"]
+
+// colors.splice(1, 0, 'yellow', 'pink')
+
+// console.log(colors); //["red", "yellow", "pink", "green", "purpl", "blue"]
+
+                      // Замена элементов массива
+
+// const languages = ['C', 'C++', 'Java', 'Javascript'];
+
+// languages.splice(1, 1, 'Python');
+
+// console.log(languages); //["C", "Python", "Java", "Javascript"]
+
+// languages.splice(2, 1, 'C#', 'Swift', 'go');
+
+// console.log(languages);
+
+                      // Метод concat()
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Monkong', 'Singu'];
+
+// const allClients = oldClients.concat(newClients);
+
+// console.log(allClients);
+
+// console.log(oldClients);
+
+// console.log(newClients);
+  
+
+                      //Функции
+
+const add = function(a, b, c) {
+    return a + b + c
+};
+
+const result = add(1, 2, 3);
+console.log(result);  //6
+
+console.log(add(5, 10, 15)); //30
