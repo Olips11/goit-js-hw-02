1180e8caa9c9fa251ee849d261e43ce1c529bd00
Chernysh Illya <olips1988@gmail.com>
2020-08-25 12:05:51 +0300

    1

---
+++ js2/task2.js
@@ -0,0 +1,57 @@
+                      // 1 - Методы Split и join
+
+//const message = 'Привет Citrus Hlobus';
+
+//console.log(message.split(' ')); //-["Привет", "Citrus", "Hlobus"]
+
+//console.log(message.split('')); //-["П", "р", "и", "в", "е", "т", " ", "C", "i", "t", "r", "u", "s", " ", "H", "l", "o", "b", "u", "s"]
+
+//const clients = ['Mango', 'Poly', 'Ajax'];
+
+//console.log(clients.join(' ')); //-Mango Poly Ajax
+//console.log(clients.join(',')); //-Mango,Poly,Ajax
+//console.log(clients.join('-')); //-Mango-Poly-Ajax
+
+                      // 2 - Метод IndexOf и includes.
+
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+
+// console.log(clients.indexOf('Ajax'));  // 1
+// console.log(clients.indexOf('Mango')); // 0
+// console.log(clients.indexOf('Gondon')); // -1
+
+// console.log(clients.includes('Ajax')); // true
+// console.log(clients.includes('Gandon')); // false
+
+                      // 3 - Проверка множуственных условий с includes()
+
+// const fruit = 'apple';
+
+// if (fruit === 'apple' || fruit === 'strawberry') {
+//     console.log('Это красный фрукт'); // Это красный фрукт
+// }
+
+// Выносим варианты в массив
+// let message;
+// const fedFruits = ['apple', 'strawberry', 'cherry', 'cranberries'];
+// const fruit = 'apple';
+
+// if( fedFruits.includes(fruit)) {
+//     console.log('Это красный фрукт'); // Это красный фрукт
+// }
+
+// if( fedFruits.includes(fruit)) {
+//     message = ('Это красный фрукт')
+// } else {
+//         message = 'Это не красный фрукт';
+//     }
+
+
+// console.log(message)
+
+
+                        // 4 - Методы push(), pop(), shift(), unshift().
+
+// push - добавляет один элемент в конце масива
+
+
