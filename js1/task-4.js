5c35863e9b5199c5f3f23942fe237f985bbbc895
Olips11 <66215413+Olips11@users.noreply.github.com>
2020-08-29 12:09:10 +0300

    1

--- js1/task-4.js
+++ js1/task-4.js
@@ -10,18 +10,19 @@
 
 function formatString(string, maxLength = 40) {
   // Write code under this line
-  if (string < maxLength) {
-    return string;
-  } else {
-    string = string.slice(0, maxLength) + '...';
+  if (string.length < maxLength) {
     return string;
   }
+  string = string.slice(0, maxLength) + '...';
+  return string;
 }
 
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // 'Curabitur ligula sapien, tincidunt non.'
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(
+  formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 45),
+);
 // 'Vestibulum facilisis, purus nec pulvinar...'
 
 console.log(
